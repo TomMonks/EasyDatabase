f7cc94c5c08a9b419078f463961ad21932fc18c7
TomMonks <thomas.m.monks@gmail.com>
2013-07-03 10:42:26 +0100

    Added DistinctSelectStatement (i.e. select distinct values) and SimpleSQLFactory (for quick creation of sql selecting data from one table)

---
+++ EasyDatabase/SQL/Select/DistinctSelectStatement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyDatabase.SQL
+{
+    public class DistinctSelectStatement : ISelectStatement 
+    {
+
+        protected const string SELECT = "SELECT DISTINCT";
+        protected const string ALL = "*";
+        protected const string SPACE = " ";
+
+        protected List<string> fields;
+
+        public DistinctSelectStatement()
+        {
+            this.fields = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string statement = SELECT;
+
+            AddSelectedFields(ref statement);
+
+            return statement;
+
+        }
+
+        
+        /// <summary>
+        /// Add a field to the select statement
+        /// </summary>
+        /// <param name="fieldName">A fieldName from the database table</param>
+        public void AddField(string fieldName){
+        	fields.Add(fieldName);
+        }
+        
+        private void AddSelectedFields(ref string statement)
+        {
+            if (fields.Count > 0)
+            {
+                foreach (string field in fields)
+                {
+                    statement = string.Concat(statement, " ", field, ",");
+                    
+                }
+                
+                statement = statement.Substring(0, statement.Length - 1);
+
+            }
+            else
+            {
+                statement = statement + SPACE + ALL;
+            }
+            
+        }
+
+        /// <summary>
+        /// Get the field name as it will appear in the SQL
+        /// </summary>
+        /// <param name="fieldName">The fieldName to return</param>
+        /// <returns>Basic select will return the fieldname in the same format</returns>
+        public string GetSQLFormatFieldName(string fieldName)
+        {
+            return fieldName;
+        }
+    }
+}
--- EasyDatabase/SQL/SimpleDistinctSQLFactory.cs
+++ EasyDatabase/SQL/SimpleDistinctSQLFactory.cs
@@ -5,35 +5,23 @@ using System.Text;
 
 namespace EasyDatabase.SQL
 {
-    public class SimpleSQLFactory : ISQLFactory 
+    public class SimpleDistinctSQLFactory : ISQLFactory 
     {
         protected string tableName;
         protected List<string> fields;
-        protected BasicSelectStatement select;
-
-        /// <summary>
-        /// Specify a select all (*) query from the specified table/view
-        /// </summary>
-        /// <param name="tableName">The table/view that contains the data</param>
-        public SimpleSQLFactory(string tableName)
-        {
-            this.tableName = tableName;
-            this.fields = new List<string>() { "*" };
-            this.select = new BasicSelectStatement();
-        }
+        protected DistinctSelectStatement select;
 
         /// <summary>
         /// Select a list of field names from the table or view
         /// </summary>
         /// <param name="tableName">The table/view that contains the data</param>
         /// <param name="fields">The list of field names containing the data to select</param>
-        public SimpleSQLFactory(string tableName, List<string> fields)
+        public SimpleDistinctSQLFactory(string tableName, List<string> fields)
         {
             this.tableName = tableName;
             this.fields = fields;
-            this.select = new BasicSelectStatement();
+            this.select = new DistinctSelectStatement();
         }
-
 
         public ISelectStatement CreateSelect()
         {
